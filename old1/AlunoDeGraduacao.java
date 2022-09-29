a2e21bbf76af81acb9e860c1d596d38b74f62e5a
Aluno <aluno@IPIRANGA.FATEC>
2022-09-29 15:34:29 -0300

    heranca(apostila05): fim da listagem 1.2.4

---
+++ old1/AlunoDeGraduacao.java
@@ -0,0 +1,5 @@
+public class AlunoDeGraduacao{
+    private String nome;
+    private int idade;
+    private double nota1, nota2, notaFinal;
+}
---
+++ old1/ProfessorPesquisador.java
@@ -0,0 +1,7 @@
+public class ProfessorPesquisador{
+    private  String nome;
+    private int idade;
+    private void pesquisar(){
+        System.out.println("Pesquisando...");
+    }
+}
